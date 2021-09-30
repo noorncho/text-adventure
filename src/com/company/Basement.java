3f0dc6858b7fd4c0478563db97ee66304f9e7592
Noor Ncho <nrandin@gmail.com>
2021-09-30 12:09:27 +0100

    separated battle logic into own class

--- src/com/company/Basement.java
+++ src/com/company/Basement.java
@@ -22,7 +22,7 @@ public class Basement {
         Scanner scan = new Scanner(System.in);
         if(scan.next().equalsIgnoreCase("L")){
             System.out.println("A dagger has been added to your inventory.");
-            player.inventory.add(new Weapon("Old Dagger", 3, 5,65));
+            player.inventory.add(new Weapon("Old Dagger", 3, 5,5));
             player.addMoney(5);
             System.out.println("Time to leave the room. Press (O) to continue");
             if(scan.next().equalsIgnoreCase("O")){
@@ -42,14 +42,14 @@ public class Basement {
         switch (choice){
             case 1:
                 System.out.println("Let the battle begin!");
-                battle();
+                fight(1);
                 System.out.println("Your first battle is Over");
                 break;
             case 2:
                 if(player.getGold() < 10){
                     System.out.println("You don't have enough money to bribe your way out.");
                     System.out.println("Prepare to fight.");
-                    battle();
+                    fight(1);
                 }else{
                     System.out.println("You were able to avoid a battle.");
                     System.out.println("Lets proceed");
@@ -57,83 +57,14 @@ public class Basement {
                     break;
                 }
         }
-    }
-
-    public void battle(){
-        int minAttack = 1;
-        int maxAttack = player.getMaxAttack();
-
-        //Give the player a chance to equip a weapon
-        if(player.getEquippedWeapon() == null && !player.inventory.isEmpty()){
-            System.out.println("Would you like to equip a weapon? (y/n) ");
-            Scanner scan = new Scanner(System.in);
-            if(scan.next().equalsIgnoreCase("y")){
-                player.showInventory();
-                System.out.println("Enter the number of the item you would like to use? ");
-                int index = scan.nextInt();
-                if(player.inventory.get(index) instanceof Weapon){
-                    player.equipWeapon(player.inventory.get(index));
-                    player.setMaxAttack(((Weapon) player.inventory.get(index)).getMaxDamage());
-                    minAttack = ((Weapon) player.getEquippedWeapon()).getMinDamage();
-                    player.inventory.remove(index);
-                }
-                System.out.println("Weapon Equipped. Fight Begin");
-            }else{
-                System.out.println("Fight Begin");
-            }
-        }
-
-        Enemy enemy = enemies[0];
-        Scanner scan = new Scanner(System.in);
-        int turn = 1;
-        Random rand = new Random();
-        int playerChoice = 0;
-
-        //Battle Logic
-        while(enemy.currentHp > 0){
-            if(player.getCurrentHp() <= 0){
-                player.alive = false;
-                break;
-            }
-
-            if(turn % 2 != 0){
-                System.out.println("player turn");
-                System.out.println("Select an option: (1)Attack (2)Block");
-                int choice = scan.nextInt();
-                playerChoice = choice;
-                switch (choice){
-                    case 1:
-                        int randomAttack = rand.nextInt(maxAttack - minAttack + 1) + minAttack;
-                        enemy.currentHp -= randomAttack;
-                        System.out.println(enemy.currentHp);
-                        System.out.println("You dealt " + randomAttack + " damage to the lv " + enemy.level +" " + enemy.name);
-                        break;
-                    case 2:
-                        System.out.println("You chose to block.");
-                        break;
-                }
-            }else{
-                System.out.println(enemy.name + " turn");
-                if(playerChoice == 2){
-                    System.out.println("You took 1 damage!");
-                    player.takeDamage(1);
-                }else{
-                    int attack = rand.nextInt(enemy.maxAttackDamage - 1 + 1) + 1;
-                    player.takeDamage(attack);
-                    System.out.println("The lv " + enemy.level + " "+ enemy.name + " dealt you " + attack + " damage.");
-                    System.out.println(player.getCurrentHp());
-                }
-            }
-            System.out.println();
-            turn++;
-        }
-
-        System.out.println("Fight Over");
 
         if(player.alive){
             System.out.println("Congratulations!!!! You survived your first battle!");
             System.out.println("Lets check your rewards!");
-            player.checkRewards(enemy);
+            player.getEquippedWeapon().reduceNumUses();
+            player.checkRewards(enemies[0]);
+            System.out.println("Not bad. Let's check your current stats.");
+            player.stats();
         }else{
             System.out.println("NOTICE: YOU HAVE DIED");
             System.out.println("Oh no! It seems you were not able to defeat your first enemy.");
@@ -141,6 +72,14 @@ public class Basement {
             System.out.println("Anyway, thanks for playing.");
             System.out.println("Bye Bye.");
         }
+
+    }
+
+    public void fight(int numEnemies){
+
+        Enemy enemy = enemies[0];
+        BattleLogic battleLogic = new BattleLogic();
+        battleLogic.battle(player, enemy);
 
     }
 }
---
+++ src/com/company/BattleLogic.java
@@ -0,0 +1,92 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class BattleLogic {
+
+    /**
+     * Battle Logic for single enemy
+     * @param player
+     * @param enemy
+     */
+    public void battle(Player player, Enemy enemy){
+        int minAttack = 1;
+        int maxAttack = player.getMaxAttack();
+
+        //Give the player a chance to equip a weapon
+        if(player.getEquippedWeapon() == null && !player.inventory.isEmpty()){
+            System.out.println("Would you like to equip a weapon? (y/n) ");
+            Scanner scan = new Scanner(System.in);
+            if(scan.next().equalsIgnoreCase("y")){
+                player.showInventory();
+                System.out.println("Enter the number of the item you would like to use? ");
+                int index = scan.nextInt();
+                if((player.inventory.get(index) instanceof Weapon) && player.inventory.get(index).isUsable()){
+                    player.equipWeapon(player.inventory.get(index));
+                    player.setMaxAttack(((Weapon) player.inventory.get(index)).getMaxDamage());
+                    minAttack = ((Weapon) player.getEquippedWeapon()).getMinDamage();
+                    player.inventory.remove(index);
+                }
+                System.out.println("Weapon Equipped. Fight Begin");
+            }else{
+                System.out.println("Fight Begin");
+            }
+        }
+        //Enemy enemy = enemies[0];
+        Scanner scan = new Scanner(System.in);
+        int turn = 1;
+        Random rand = new Random();
+        int playerChoice = 0;
+
+        //Battle Logic
+        while(enemy.currentHp > 0){
+            if(player.getCurrentHp() <= 0){
+                player.alive = false;
+                break;
+            }
+
+            if(turn % 2 != 0){
+                System.out.println("player turn");
+                System.out.println("Select an option: (1)Attack (2)Block");
+                int choice = scan.nextInt();
+                playerChoice = choice;
+                switch (choice){
+                    case 1:
+                        int randomAttack = rand.nextInt(maxAttack - minAttack + 1) + minAttack;
+                        enemy.currentHp -= randomAttack;
+
+                        //System.out.println("Player: " + player.getCurrentHp() + "/" + player.getMaxHp());
+                        //System.out.println("Enemy: " + enemy.currentHp + "/" + enemy.maxHp);
+
+                        System.out.println("You dealt " + randomAttack + " damage to the lv " + enemy.level +" " + enemy.name);
+                        break;
+                    case 2:
+                        System.out.println("You chose to block.");
+                        break;
+                }
+
+                System.out.println("Player: " + player.getCurrentHp() + "/" + player.getMaxHp());
+                System.out.println("Enemy: " + enemy.currentHp + "/" + enemy.maxHp);
+            }else{
+                System.out.println(enemy.name + " turn");
+                if(playerChoice == 2){
+                    System.out.println("You took 1 damage!");
+                    player.takeDamage(1);
+                }else{
+                    int attack = rand.nextInt(enemy.maxAttackDamage - 1 + 1) + 1;
+                    player.takeDamage(attack);
+                    System.out.println("The lv " + enemy.level + " "+ enemy.name + " dealt you " + attack + " damage.");
+
+                }
+                System.out.println("Player: " + player.getCurrentHp() + "/" + player.getMaxHp());
+                System.out.println("Enemy: " + enemy.currentHp + "/" + enemy.maxHp);
+            }
+
+            System.out.println();
+            turn++;
+        }
+
+        System.out.println("Fight Over");
+    }
+}
